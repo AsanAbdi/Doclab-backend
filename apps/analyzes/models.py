bea5a8ee495eb6585dfd636676ee0db6e207795a
AsanAbdi <asanabdi50@gmail.com>
2024-07-28 17:35:41 +0100

    added _docs for doctors

--- apps/analyzes/models.py
+++ apps/analyzes/models.py
@@ -6,12 +6,19 @@ class Analyze(models.Model):
         verbose_name='Название',
         max_length=150,
     )
+    name_docs = models.CharField(
+        verbose_name='Название докторам',
+        max_length=150,
+    )
     price = models.PositiveIntegerField(
         verbose_name='Цена',
         default=0,
     )
     description = models.TextField(
         verbose_name='Описание',
+    )
+    description_docs = models.TextField(
+        verbose_name='Описание докторам'
     )
     is_actual = models.BooleanField(
         default=True,
